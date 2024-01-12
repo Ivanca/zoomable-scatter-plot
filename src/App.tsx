996178b6882c247e11e2bfbcd9ed72c22661dd0f
Ivan Castellanos <ivanca@gmail.com>
2024-01-12 00:19:44 -0500

    Basic scatter plot using d3 working Based on https://www.react-graph-gallery.com/scatter-plot

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ScatterPlot } from './components/ScatterPlot';
 
 function App() {
+  const [data, setData] = React.useState([]);
+  // import data from "./data.json";
+  useEffect(() => {
+    fetch("./data.json")
+      .then(res => res.json())
+      .then(data => {
+        setData(data);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ScatterPlot data={data} width={400} height={400} />
   );
 }
 
